3a768dc4d2a51930e94c2658f4f08e6f469c1c6c
Adryan da Silva <adryandasilva3@gmail.com>
2019-06-01 09:23:09 -0300

    Ligação do projeto ao banco de dados

---
+++ DAL/ConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using System.Data;
+using Npgsql;
+
+namespace DAL
+{
+    public static class ConnectionFactory
+    {
+        public static NpgsqlConnection Connect()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection
+            ("Server=localhost; Port=5433;Userid=Postgres;password=12345;Database=CSHARP;");
+            conn.Open();
+            return conn;
+
+
+        }
+    }
+}
